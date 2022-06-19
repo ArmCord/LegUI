80d42ddbc6a3e25d3249bbf64b68eebc658962e8
smartfrigde <37928912+smartfrigde@users.noreply.github.com>
2022-06-19 17:31:14 +0200

    Change the import name to avoid issues with newer Nim

--- test.nim
+++ test.nim
@@ -1,13 +1,5 @@
 import webview
 
-var wt = newWebview("TEST", "data:text/html, <h1>It worked! Yay!</h1>", debug=true)
+var wt = newWebview("Legcord", "https://discord.com/app", debug=true)
 
-# proc receiver(seq: cstring, req: cstring, arg: pointer) {.cdecl.} =
-#     echo "SEQ: " & $(seq)
-#     echo "GOT: " & $(req)
-#     echo "arg: " & $(cast[int](arg))
-
-#     wt.webview_return(seq, 0.cint, ($ %*("this is a message")).cstring)
-
-# wt.webview_bind("something", receiver, cast[pointer](666))
 wt.show()
